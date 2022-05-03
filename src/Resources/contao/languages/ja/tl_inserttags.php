ab4dc299944aed5dca78864a6e40c1bf1ac8e099
Takahiro Kambe <taca@back-street.net>
2022-05-03 16:23:56 +0900

    Japanese translation (#22)

---
+++ src/Resources/contao/languages/ja/tl_inserttags.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of terminal42/contao-inserttags.
+ *
+ * (c) terminal42
+ *
+ * @license MIT
+ */
+
+// Fields
+$GLOBALS['TL_LANG']['tl_inserttags']['tag'] = ['挿入タグ', '挿入タグを入力してください。<br>{{custom::your-tag}}を使用して、どのコンテント要素でもこのタグを使用できます。'];
+$GLOBALS['TL_LANG']['tl_inserttags']['replacement'] = ['置き換え', '挿入タグと置き換える内容を入力してください。'];
+$GLOBALS['TL_LANG']['tl_inserttags']['disableRTE'] = ['TinyMCEを不使用', 'リッチテキストエディタTinyMCEを無効にしたい場合に選択してください。'];
+$GLOBALS['TL_LANG']['tl_inserttags']['description'] = ['説明', '挿入タグの説明を入力して、同じタグを相互に区別できます。'];
+$GLOBALS['TL_LANG']['tl_inserttags']['limitpages'] = ['ページを限定', '挿入タグを特定のページに限定したい場合に選択してください。'];
+$GLOBALS['TL_LANG']['tl_inserttags']['pages'] = ['ページ', '挿入タグを限定して適用するページを選択してください。'];
+$GLOBALS['TL_LANG']['tl_inserttags']['includesubpages'] = ['下位のページを含める', '選択したページの下位のページにも挿入タグを適用したい場合に選択してください。'];
+$GLOBALS['TL_LANG']['tl_inserttags']['protected'] = ['挿入タグを保護', '挿入タグを特定のメンバーグループにだけ表示します。'];
+$GLOBALS['TL_LANG']['tl_inserttags']['groups'] = ['許可するメンバーグループ', 'これらのグループは挿入タグを見ることができます。'];
+
+// Buttons
+$GLOBALS['TL_LANG']['tl_inserttags']['new'] = ['タグを作成', '新しい挿入タグを作成'];
+$GLOBALS['TL_LANG']['tl_inserttags']['edit'] = ['タグを編集', 'ID %sの挿入タグを編集'];
+$GLOBALS['TL_LANG']['tl_inserttags']['copy'] = ['タグを複製', 'ID %sの挿入タグを複製'];
+$GLOBALS['TL_LANG']['tl_inserttags']['cut'] = ['タグを移動', 'ID %sの挿入タグを移動'];
+$GLOBALS['TL_LANG']['tl_inserttags']['delete'] = ['タグを削除', 'ID %sの挿入タグを削除'];
+$GLOBALS['TL_LANG']['tl_inserttags']['show'] = ['タグの詳細', 'ID %sの挿入タグの詳細を表示'];
+$GLOBALS['TL_LANG']['tl_inserttags']['pasteafter'] = ['後に貼付け', 'ID %sの挿入タグの後に貼付け'];
+$GLOBALS['TL_LANG']['tl_inserttags']['pasteinto'] = ['中に貼り付け', '戦闘に貼り付け'];
+
+// Legends
+$GLOBALS['TL_LANG']['tl_inserttags']['tag_legend'] = 'タグと置き換え';
+$GLOBALS['TL_LANG']['tl_inserttags']['limit_legend'] = '限定';
