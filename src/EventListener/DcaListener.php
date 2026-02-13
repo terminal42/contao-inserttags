058cca0b21150ddb7c1600e39134375d060f4747
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2026-02-13 13:59:55 +0100

    Remove legacy code

--- src/EventListener/DcaListener.php
+++ src/EventListener/DcaListener.php
@@ -4,11 +4,8 @@ declare(strict_types=1);
 
 namespace Terminal42\InsertTagsBundle\EventListener;
 
-use Composer\InstalledVersions;
-use Composer\Semver\VersionParser;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsCallback;
 use Contao\DC_Table;
-use Contao\Input;
 use Doctrine\DBAL\Connection;
 
 class DcaListener
@@ -21,36 +18,11 @@ class DcaListener
     #[AsCallback(table: 'tl_inserttags', target: 'fields.replacementNot.attributes')]
     public function disableRte(array $attributes, DC_Table $dc): array
     {
-        /** @phpstan-ignore-next-line */
-        if (method_exists($dc, 'getActiveRecord') && ($dc->getActiveRecord()['disableRTE'] ?? false)) {
+        if ($dc->getActiveRecord()['disableRTE'] ?? false) {
             unset($attributes['rte']);
         }
 
         return $attributes;
-    }
-
-    #[AsCallback(table: 'tl_inserttags', target: 'config.onload')]
-    public function onLoadCallback(DC_Table $dc): void
-    {
-        // Contao 5.1 introduced the attributes callback which we'll use instead because it also works in edit-multiple
-        if (
-            /** @phpstan-ignore-next-line */
-            method_exists($dc, 'getActiveRecord')
-            && class_exists(InstalledVersions::class)
-            && class_exists(VersionParser::class)
-            && InstalledVersions::satisfies(new VersionParser(), 'contao/core-bundle', '^5.1')
-        ) {
-            return;
-        }
-
-        // Disable rich text editor if checkbox is set
-        if ('edit' === Input::get('act')) {
-            $disableRTE = $this->connection->fetchOne("SELECT disableRTE FROM {$dc->table} WHERE id=?", [$dc->id]);
-
-            if ($disableRTE) {
-                unset($GLOBALS['TL_DCA'][$dc->table]['fields']['replacement']['eval']['rte'], $GLOBALS['TL_DCA'][$dc->table]['fields']['replacementNot']['eval']['rte']);
-            }
-        }
     }
 
     #[AsCallback(table: 'tl_inserttags', target: 'fields.groups.options')]
